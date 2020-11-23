dbc184a103f410959ea86ffd7b216b88d8f1507b
bmlmcmc <44882493+bmlmcmc@users.noreply.github.com>
2020-11-23 13:00:57 +0700

    FGWC classical algorithm

--- R/fgwc.R
+++ R/fgwc.R
@@ -2,6 +2,51 @@
 #####################CLASSICAL FGWC#####################
 ########################################################
 library(rdist)
+
+#' Classical Fuzzy Geographicaly Weighted Clustering
+#' @description Fuzzy clustering with addition of spatial configuration of membership matrix
+#' @param data an object of data with d>1. Can be \code{matrix} or \code{data.frame}. If your data is univariate, bind it with \code{1} to get a 2 columns.
+#' @param pop an n*1 vector contains population.
+#' @param distmat an n*n distance matrix between regions.
+#' @param kind use \code{'u'} if you want to use membership approach and \code{'v'} for centroid approach.
+#' @param m degree of fuzziness or fuzzifier. Default is 2.
+#' @param distance the distance metric between data and centroid, the default is euclidean, see \code{\link{cdist}} for details.
+#' @param order, minkowski order. default is 2.
+#' @param alpha the old membership effect with [0,1], if \code{alpha} equals 1, it will be same as fuzzy C-Means, if 0, it equals to neighborhood effect.
+#' @param a spatial magnitude of distance. Default is 1.
+#' @param b spatial magnitude of population. Default is 1.
+#' @param max.iter maximum iteration. Default is 500.
+#' @param error error tolerance. Default is 1e-5.
+#' @param randomN random seed for initialisation (if uij or vi is NA). Default is 0.
+#' @param uij membership matrix initialisation.
+#' @param vi centroid matrix initialisation.
+
+#' @return an object of class \code{"fgwc"}.\cr
+#' An \code{"fgwc"} object contains as follows:
+#' \itemize{
+#' \item \code{converg} - the process convergence of objective function
+#' \item \code{f_obj} - objective function value
+#' \item \code{membership} - membership matrix
+#' \item \code{centroid} - centroid matrix
+#' \item \code{validation} - validation indices (there are partition coefficient (\code{PC}), classification entropy (\code{CE}), 
+#' SC index (\code{SC}), separation index (\code{SI}), Xie and Beni's index (\code{XB}), IFV index (\code{IFV}), and Kwon index (Kwon))
+#' \item \code{max.iter} - Maximum iteration
+#' \item \code{cluster} - the cluster of the data
+#' \item \code{finaldata} - The final data (with the cluster)
+#' \item \code{call} - the syntax called previously
+#' \item \code{time} - computational time.
+#' }
+#' @details Fuzzy Geographically Weighted Clustering (FGWC) was developed by Mason and Jacobson (2007) by adding 
+#' neighborhood effects and population to configure the membership matrix in Fuzzy C-Means.
+
+#' @seealso \code{\link{fpafgwc,ifafgwc}}
+#' @examples
+#' data('census2010')
+#' data('census2010dist')
+#' data('census2010pop')
+#' fgwcuv(census2010,census2010pop,census2010dist,'u',3,2,'euclidean',4)
+#'
+
 fgwcuv <- function(data, pop, distmat, kind=NA,ncluster=2, m=2, distance='euclidean', order=2,
                       alpha=0.7, a=1, b=1, max.iter=500, error=1e-5,
                      randomN=0, uij=NA, vi=NA) {
@@ -71,9 +116,10 @@ fgwcuv <- function(data, pop, distmat, kind=NA,ncluster=2, m=2, distance='euclid
   fgwc_obj <- sum(new_uij^m*uij$d) ##fungsi objektif akhir
   finaldata <- determine_cluster(data,new_uij)
   cluster <- finaldata[,ncol(finaldata)]
-  result <- list("converg"=conv[-1],"f_obj"=fgwc_obj, "membership"=new_uij,"centroid"=vi, "validasi"=index_fgwc(data,cluster,new_uij,vi,m,exp(1)) ,"max.iter" = iter,
+  result <- list("converg"=conv[-1],"f_obj"=fgwc_obj, "membership"=new_uij,"centroid"=vi, "validation"=index_fgwc(data,cluster,new_uij,vi,m,exp(1)) ,"iteration" = iter,
                    "cluster"=cluster, "finaldata"=finaldata, "call"=match.call(), "time" = proc.time()-ptm)
   print(c(order, ncluster,m, randomN))
+  class(result) <- 'fgwc'
   return (result)
 }
 
@@ -183,135 +229,4 @@ jfgwcv2  <- function(data,vi,m,distance,order,mi.mj,dist,alpha,beta,a,b) { ##fun
     }
   }
   return(sum((u^m)*d))
-}
-
-##penghitungan index
-index_fgwc <- function(data,cluster,uij,vi,m,a=exp(1)) {
-  result<-list()
-  result$PC <- PC1(uij)
-  result$CE <- CE1(uij,a)
-  result$SC <- SC1(data,cluster,uij,vi,m)
-  result$SI <- SI1(data,uij,vi)
-  result$XB <- XB1(data,uij,vi,m)
-  result$IFV <- IFV1(data,uij,vi,m)
-  result$Kwon <- Kwon1(data,uij,vi,m)
-  return(result)
-}
-
-########################################################
-#################VALIDATION MEASUREMENT#################
-########################################################
-
-##kelompok yang optimum dinyatakan dengan nilai PC yang maksimum.
-PC1 <- function(uij) {
-  return(sum(uij^2)/nrow(uij))
-}
-
-##kelompok yang optimum dinyatakan dengan nilai indeks CE yang minimum.
-CE1 <- function(uij,a=exp(1)) {##
-  return(sum(uij*log(uij,a))/(-nrow(uij)))
-}
-
-##Partisi yang optimum dinyatakan dengan nilai indeks SC yang minimum.
-SC1 <- function(data,cluster,uij,vi,m) {
-  d <- matrix(0,nrow(data),nrow(vi))
-  for (i in 1:nrow(data)) {
-    for (j in 1:nrow(vi)) {
-      d[i,j] <- sum((vi[j,]-data[i,])^2)
-    }
-  }
-  pt1 <- colSums((uij^m)*d)
-  vkvi <- matrix(0,nrow(vi),nrow(vi))
-  for (i in 1:nrow(vi)) {
-    for (k in 1:nrow(vi)) {
-      vkvi[i,k] <- sum((vi[i,]-vi[k,])^2)
-    }
-    Ni <- length(which(cluster==i))
-    vkvi[i,]<-Ni*vkvi[i,]
-  }
-  pt2 <- colSums(vkvi)
-  return(sum(pt1/pt2))
-}
-
-##Jumlah kelompok yang optimum dinyatakan dengan nilai indeks S yang minimum.
-SI1 <- function(data,uij,vi) {
-  d <- matrix(0,nrow(data),nrow(vi))
-  for (i in 1:nrow(data)) {
-    for (j in 1:nrow(vi)) {
-      d[i,j] <- sum((vi[j,]-data[i,])^2)
-    }
-  }
-  vkvi <- matrix(0,nrow(vi),nrow(vi))
-  for (i in 1:nrow(vi)) {
-    for (k in 1:nrow(vi)) {
-      vkvi[i,k] <- sum((vi[k,]-vi[i,])^2)
-    }
-  }
-  diag(vkvi) <- Inf
-  pt1 <- sum((uij^2)*d)
-  pt2 <- nrow(data)*min(vkvi)
-  return(sum(pt1/pt2))
-}
-
-##Jumlah kelompok yang optimal dinyatakan dengan nilai XB yang minimum.
-XB1 <- function(data,uij,vi,m) {
-  d <- matrix(0,nrow(data),nrow(vi))
-  for (i in 1:nrow(data)) {
-    for (j in 1:nrow(vi)) {
-      d[i,j] <- sum((vi[j,]-data[i,])^2)
-      if (d[i,j]==0) {
-        d[i,j]==Inf
-      }
-    }
-  }
-  pt1 <- sum((uij^m)*d)
-  pt2 <- nrow(data)*min(d)
-  return(pt1/pt2)
-}
-
-##Ketika nilai IFV maksimum maka kualitas cluster semakin baik.
-IFV1 <- function(data,uij,vi,m) {
-  vkvi <- matrix(0,nrow(vi),nrow(vi))
-  for (i in 1:nrow(vi)) {
-    for (k in 1:nrow(vi)) {
-      vkvi[i,k] <- sum((vi[k,]-vi[i,])^2)
-    }
-  }
-  d <- matrix(0,nrow(data),nrow(vi))
-  for (i in 1:nrow(data)) {
-    for (j in 1:nrow(vi)) {
-      d[i,j] <- sum((vi[j,]-data[i,])^2)
-    }
-  }
-  sigmaD <- sum(d)/(nrow(data)*nrow(vi))
-  SDmax <- max(vkvi)
-  log2u <- colSums(log(uij,2))/nrow(data)
-  u2ij <- colSums(uij^2)
-  inside <- sum(u2ij*(log(nrow(vi),2)-log2u))
-  return(sum(u2ij*((log(nrow(vi),2)-log2u)^2)/nrow(data)*(SDmax/sigmaD)))
-}
-
-##Ketika nilai Kwon minimum maka kualitas cluster semakin baik.
-Kwon1 <- function(data,uij,vi,m) {
-  d <- matrix(0,nrow(data),nrow(vi))
-  s <- matrix(0,nrow(vi))
-  vivj <- matrix(0,nrow(vi),nrow(vi))
-  for (j in 1:nrow(vi)) {
-    s[j,] <- (sum(vi[j,]-colMeans(data))^2)
-  }
-  for (i in 1:nrow(data)) {
-    for (j in 1:nrow(vi)) {
-      d[i,j] <- sum((vi[j,]-data[i,])^2)
-    }
-  }
-  for (i in 1:nrow(vi)) {
-    for (j in 1:nrow(vi)) {
-      vivj[i,j] <- sum((vi[i,]-vi[j,])^2)
-    }
-  }
-  diag(vivj) <- Inf
-  pt1 <- colSums((uij^m)*d)
-  pt2 <- sum(s)/nrow(vi)
-  pt3 <- min(vivj)
-  return(sum((pt1+pt2)/pt3))
 }
