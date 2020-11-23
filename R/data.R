f5536061dd6bedda2e5b483d4345e142e92f7d93
bmlmcmc <44882493+bmlmcmc@users.noreply.github.com>
2020-11-23 13:00:40 +0700

    Create data.R

---
+++ R/data.R
@@ -0,0 +1,63 @@
+#' Indonesia 2010 Provincial Census data
+#'
+#' A dataset containing the demographic characteristics of Indonesia on provincial level
+#' from BPS-Statistics Indonesia
+#' @format A data frame with 33 rows and 110 demographic characteristics.\cr
+#' The dataset was firstly used in Wijayanto (2014), Wijayanto and Purwarianti (2014) and Wijayanto et al. (2015).
+#' @source
+#' \itemize{
+#'    \item BPS, Statistics Indonesia - 2010 Population Census, Available: sp2010.bps.go.id.
+#'    \item Wijayanto, Arie Wahyu. (2015). 
+#'          Improvement of Fuzzy Geo-Demographic Clustering using Metaheuristic Optimization on Indonesia Population Census. 
+#'    \item Wijayanto, A.W., Purwarianti, A., Son, L.H., 
+#'    Fuzzy geographically weighted clustering using artificial bee colony: An efficient geo-demographic 
+#'    analysis algorithm and applications to the analysis of crime behavior in population. 
+#'    Applied Intelligence 44(2), 377–398 (2015).
+#'    \item Wijayanto, A.W., Purwarianti, A., 
+#'    Improvement design of fuzzy geo-demographic clustering using artificial bee colony optimization. 
+#'    In: 2014 International Conference on Cyber and IT Service Management (CITSM), 1, pp. 69–74. IEEE (2014).
+#' }
+#' 
+"census2010"
+
+#' Indonesia Provincial Matrix Distance
+#'
+#' A dataset containing the calculated distance between provinces in Indonesia.
+#' @format A matrix with 33x33 rows.\cr
+#' The dataset was firstly used in Wijayanto (2014), Wijayanto and Purwarianti (2014) and Wijayanto et al. (2015)
+#' @source
+#' \itemize{
+#'    \item BPS, Statistics Indonesia - 2010 Population Census, Available: sp2010.bps.go.id.
+#'    \item Wijayanto, Arie Wahyu. (2015). 
+#'          Improvement of Fuzzy Geo-Demographic Clustering using Metaheuristic Optimization on Indonesia Population Census. 
+#'    \item Wijayanto, A.W., Purwarianti, A., Son, L.H., 
+#'    Fuzzy geographically weighted clustering using artificial bee colony: An efficient geo-demographic 
+#'    analysis algorithm and applications to the analysis of crime behavior in population. 
+#'    Applied Intelligence 44(2), 377–398 (2015).
+#'    \item Wijayanto, A.W., Purwarianti, A., 
+#'    Improvement design of fuzzy geo-demographic clustering using artificial bee colony optimization. 
+#'    In: 2014 International Conference on Cyber and IT Service Management (CITSM), 1, pp. 69–74. IEEE (2014).
+#' }
+#' 
+"census2010dist"
+
+#' Indonesia 2010 Population
+#'
+#' A dataset containing the 2010 population in Indonesia based on census result.
+#' @format A vector with 33 elements.\cr
+#' The dataset was firstly used in Wijayanto (2014), Wijayanto and Purwarianti (2014) and Wijayanto et al. (2015)
+#' @source
+#' \itemize{
+#'    \item BPS, Statistics Indonesia - 2010 Population Census, Available: sp2010.bps.go.id.
+#'    \item Wijayanto, Arie Wahyu. (2015). 
+#'          Improvement of Fuzzy Geo-Demographic Clustering using Metaheuristic Optimization on Indonesia Population Census. 
+#'    \item Wijayanto, A.W., Purwarianti, A., Son, L.H., 
+#'    Fuzzy geographically weighted clustering using artificial bee colony: An efficient geo-demographic 
+#'    analysis algorithm and applications to the analysis of crime behavior in population. 
+#'    Applied Intelligence 44(2), 377–398 (2015).
+#'    \item Wijayanto, A.W., Purwarianti, A., 
+#'    Improvement design of fuzzy geo-demographic clustering using artificial bee colony optimization. 
+#'    In: 2014 International Conference on Cyber and IT Service Management (CITSM), 1, pp. 69–74. IEEE (2014).
+#' }
+#' 
+"census2010pop"
