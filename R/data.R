be7ddd05a61d3041bd9a645a582838c0a7a2a00d
bmlmcmc <44882493+bmlmcmc@users.noreply.github.com>
2021-03-08 22:04:12 +0700

    documentation update
    documentation update

--- R/data.R
+++ R/data.R
@@ -3,7 +3,7 @@
 #' A dataset containing the demographic characteristics of Indonesia on provincial level
 #' from BPS-Statistics Indonesia
 #' @details A data frame with 33 rows and 110 demographic characteristics.\cr
-#' The dataset was used in \insertCite{fgwcpso;textual}{naspaclust}, \insertCite{fgwcabc1;textual}{naspaclust} and \insertCite{fgwcabc2;textual}{naspaclust}.\cr
+#' The dataset was used in \insertCite{Wijayanto2015a;textual}{naspaclust}, \insertCite{fgwcpso;textual}{naspaclust}, \insertCite{fgwcabc1;textual}{naspaclust} and \insertCite{fgwcabc2;textual}{naspaclust}.\cr
 #' The details of each variable can be seen in \insertCite{Wijayanto2015a;textual}{naspaclust}, 
 #' @references
 #' \insertAllCited{}
@@ -14,7 +14,7 @@
 #'
 #' A dataset containing the calculated distance between provinces in Indonesia.
 #' @details A matrix with 33x33 rows.\cr
-#' The dataset was firstly used in \insertCite{Wijayanto2015a;textual}{naspaclust}, \insertCite{fgwcpso;textual}{naspaclust}, \insertCite{fgwcabc1;textual}{naspaclust} and \insertCite{fgwcabc2;textual}{naspaclust}.
+#' The dataset was firstly used in \insertCite{Wijayanto2015a;textual}{naspaclust}, \insertCite{fgwcpso;textual}{naspaclust}, \insertCite{fgwcabc1;textual}{naspaclust}, and \insertCite{fgwcabc2;textual}{naspaclust}.
 #' @references
 #' \insertAllCited{}
 
@@ -25,7 +25,7 @@
 #'
 #' A dataset containing the 2010 population in Indonesia based on census result.
 #' @details A vector with 33 elements.\cr
-#' The dataset was firstly used in \insertCite{Wijayanto2015a;textual}{naspaclust}, \insertCite{fgwcpso;textual}{naspaclust}, \insertCite{fgwcabc1;textual}{naspaclust} and \insertCite{fgwcabc2;textual}{naspaclust}.
+#' The dataset was firstly used in \insertCite{Wijayanto2015a;textual}{naspaclust}, \insertCite{fgwcpso;textual}{naspaclust}, \insertCite{fgwcabc1;textual}{naspaclust}, and \insertCite{fgwcabc2;textual}{naspaclust}.
 #' @references
 #' \insertAllCited{}
 #' 
